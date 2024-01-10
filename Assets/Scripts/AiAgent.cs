d221afcf5a57884ff891f440b5532f2541ee3369
MarcusReyes619 <129239355+MarcusReyes619@users.noreply.github.com>
2024-01-09 23:31:50 -0700

    first push

---
+++ Assets/Scripts/AiAgent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiAgent : MonoBehaviour
+{
+	public AiMovement movement;
+
+
+}
+
---
+++ Assets/Scripts/AiDistancePerepction.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiDistancePerepction : AiPerception
+{
+	public override GameObject[] GetGameObjects()
+	{
+		throw new System.NotImplementedException();
+	}
+}
